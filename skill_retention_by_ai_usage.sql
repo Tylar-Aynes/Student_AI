2eaa28417e64efa75b039c944535a2b0f8ece0b4
Tylar-Aynes <170891031+Tylar-Aynes@users.noreply.github.com>
2026-06-29 04:40:23 +0200

    Update skill_retention_by_ai_usage.sql

--- skill_retention_by_ai_usage.sql
+++ skill_retention_by_ai_usage.sql
@@ -1,6 +1,6 @@
 SELECT 
   ROUND(AVG(traditional_study_hours), 3) AS avg_traditional_study,
-  ROUND(AVG(weekly_gen_ai_hours), 3) avg_gen_ai_usage,
+  ROUND(AVG(weekly_gen_ai_hours), 3) AS avg_gen_ai_usage,
   CASE 
     WHEN skill_retention_score < 33.4 THEN 'Low' WHEN skill_retention_score < 66.7 THEN 'Medium' ELSE 'High' END AS skill_retention_category 
 FROM ai_impact 
